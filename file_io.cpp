eb69018204696ffb28c78b74e6e6fc45e4ec312d
Utku SENOCAK <u.senocakk@gmail.com>
2021-01-25 21:22:11 +0300

    Add example of file input output

---
+++ file_io.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+constexpr int maxstring = 1024; //read buffer size
+constexpr int repeat = 5;
+
+int main( int argc, char ** argv)
+{
+	const char* fn = "testfile.txt";
+	const char* str = "This is a literal c-string.\n";
+
+	// create/write the file
+	printf("writing file\n");
+	FILE* fw = fopen(fn, "w");
+	for (int i = 0; i < repeat; i++) {
+		fputs(str, fw);
+	}
+
+	fclose(fw);
+	printf("done.\n");
+
+	//read the file
+	printf("reading file\n");
+	char buf[maxstring];
+	FILE* fr = fopen(fn, "r");
+	while (fgets(buf, maxstring, fr))
+	{
+		fputs(buf, stdout);
+	}
+
+	fclose(fr);
+	remove(fn);
+
+	printf("done.\n");
+
+	return 0;
+}
