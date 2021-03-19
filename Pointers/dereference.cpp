5d8404f26ec9ccb0fe833cf0107896c5e5446f9d
Utku SENOCAK <u.senocakk@gmail.com>
2021-03-19 15:58:52 +0300

    Add example of dereferencing pointers

---
+++ Pointers/dereference.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	double testScores[5], sum = 0;
+	double* pTestScores;
+	pTestScores = testScores;
+	for (int i = 0; i < 5; i++)
+	{
+		cout << "Enter the test score: " << endl;
+		cin >> *(pTestScores + i);
+		sum += *(pTestScores + i);
+	}
+	cout << "testScores: " << testScores << endl;
+	cout << "Total for all students: " << sum << endl;
+	cout << "Average score: " << sum / 5 << endl;
+
+	return 0;
+}
